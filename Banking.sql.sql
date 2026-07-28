8908d2a47df94d091ee0805c4f40932470751602
shreyaiv <shreyaiw24@gmail.com>
2026-07-28 08:53:51 +0530

    created bankingdb database

---
+++ Banking.sql.sql
@@ -0,0 +1,15 @@
+-- show databases;
+-- create database BankingDB;
+USE bankingdb;
+CREATE TABLE Customers (
+CustomerID INT PRIMARY KEY,
+FirstName VARCHAR (50),
+LastName VARCHAR(50),
+Email VARCHAR(100),
+Phone VARCHAR(15),
+AccountCreationDate DATE
+);
+
+desc customers;
+
+select * from customers;
